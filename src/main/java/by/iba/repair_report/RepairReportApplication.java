0bc826f1c2c2d3a40d8ffb5f04d2fb519de6b329
CharnenkaTatsiana <charnenka1.23@gmail.com>
2025-08-18 21:01:18 +0300

    Initial import from Spring Initializr

---
+++ src/main/java/by/iba/repair_report/RepairReportApplication.java
@@ -0,0 +1,13 @@
+package by.iba.repair_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/iba/repair_report/RepairReportApplicationTests.java
@@ -0,0 +1,13 @@
+package by.iba.repair_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
